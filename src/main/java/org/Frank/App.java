c204c52a60189a54c0c1153efa66e4487924808d
FOM008 <118181863+FOM008@users.noreply.github.com>
2023-12-07 13:44:17 +0300

    updated heroes&squadLists display

--- src/main/java/org/Frank/App.java
+++ src/main/java/org/Frank/App.java
@@ -43,8 +43,65 @@ public class App {
             return new ModelAndView(model, "squadform.hbs");
         }, new HandlebarsTemplateEngine());
 
+        //creating new squad page
+        get("/squads/new", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            List<Hero> heroesWithNoSquad = new ArrayList<>();
+            for (Hero hero : Hero.getHeroList()) {
+                if (hero.getSquadGroup().equals("")) {
+                    heroesWithNoSquad.add(hero);
+                }
+            }
+            model.put("heroesWithNoSquad", heroesWithNoSquad);
+            return new ModelAndView(model, "squadform.hbs");
+        }, new HandlebarsTemplateEngine());
 
+        //create a new squad page - redirect to success page
+        post("/squads/new", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            List<Hero> heroesWithNoSquad = new ArrayList<>();
+            for (Hero hero : Hero.getHeroList()) {
+                if (hero.getSquadGroup().equals("")) {
+                    heroesWithNoSquad.add(hero);
+                }
+            }
+            String name = request.queryParams("squadName");
+            String cause = request.queryParams("squadCause");
+            String heroName = request.queryParams("founder");
+            Hero squadFounder = null;
+            for (Hero hero : heroesWithNoSquad) {
+                if (hero.getName().equalsIgnoreCase(heroName)) {
+                    squadFounder = hero;
+                    break;
+                }
+            }
+            assert squadFounder != null;
+            Squad newSquad = new Squad(name, cause, squadFounder);
+            return new ModelAndView(model, "success.hbs");
+        }, new HandlebarsTemplateEngine());
 
+        post("/success", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            String uniqueId = request.queryParams("uniqueId");
+            request.session().attribute("uniqueId", uniqueId);
+            model.put("uniqueId", uniqueId);
+            return new ModelAndView(model, "success.hbs");
+        }, new HandlebarsTemplateEngine());
 
+        //display heroes list
+        get("/heroes", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            model.put("hero", Hero.getHeroList());
+            model.put("uniqueId", request.session().attribute("uniqueId"));
+            return new ModelAndView(model, "herolist.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        //display squads list
+        get("/squads", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            model.put("squad", Squad.getSquadsList());
+            model.put("uniqueId", request.session().attribute("uniqueId"));
+            return new ModelAndView(model, "squadlist.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }
